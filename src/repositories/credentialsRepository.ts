3cd2a89d8b86864c1dfa2920790e20615dabbfb3
Jackie-Moraes <patrickandradepatrick@hotmail.com>
2022-07-15 16:50:20 -0300

    fix: encrypting password

--- src/repositories/credentialsRepository.ts
+++ src/repositories/credentialsRepository.ts
@@ -1,5 +1,9 @@
+import Cryptr from "cryptr"
+
 import { client } from "../config/database.js"
 import { credentialsData } from "../services/credentialsService.js"
+
+const cryptr = new Cryptr(process.env.CRYPTR_SECRET)
 
 async function checkIfTitleIsDuplicate(title: string, userId: number) {
     const exists = await client.credentials.findFirst({
@@ -12,13 +16,15 @@ async function checkIfTitleIsDuplicate(title: string, userId: number) {
 }
 
 async function createNewCredential(body: credentialsData, userId: number) {
+    const encryptPassword = cryptr.encrypt(body.password)
+
     await client.credentials.create({
         data: {
             userId,
             title: body.title,
             url: body.url,
             username: body.username,
-            password: body.password,
+            password: encryptPassword,
         },
     })
 }
