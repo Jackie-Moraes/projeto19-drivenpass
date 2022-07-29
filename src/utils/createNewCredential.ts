07581473396c8e59667c7fdd37f1ce2918cef233
Vinicius <vinigugelmin@hotmail.com>
2022-07-29 16:37:17 -0300

    Preparando revisão de código

--- src/utils/createNewCredential.ts
+++
@@ -1,9 +0,0 @@
-import { credentialsRepository } from "../repositories/credentialsRepository.js"
-import { credentialsData } from "../services/credentialsService.js"
-
-export async function createNewCredential(
-    body: credentialsData,
-    userId: number
-) {
-    await credentialsRepository.createNewCredential(body, userId)
-}
